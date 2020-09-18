ae1a1f1772599205972d517b9631a7fad2e14016
raspberry <povilas.urbonas85@gmail.com>
2020-09-18 07:20:40 +0100

    rererere

---
+++ git_script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+git config --global user.email "povilas.urbonas85@gmail.com"
+echo "Enter your message"
+read message
+git add .
+git commit -m"${message}"
+if [ -n "$(git status - porcelain)" ];
+then
+ echo "IT IS CLEAN"
+else
+ git status
+ echo "Pushing data to remote server!!!"
+ git push -u origin master
+fi
