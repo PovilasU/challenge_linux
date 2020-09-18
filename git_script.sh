6ee70d30c2ed7f80952647b83ef477a66fbf59bd
raspberry <povilas.urbonas85@gmail.com>
2020-09-18 07:50:07 +0100

    zzzzzzz

--- git_script.sh
+++ git_script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "hello" >> README.md
 
-git config --global user.email "povilas.urbonas85@gmail.com"
+#git config --global user.email "povilas.urbonas85@gmail.com"
 
 echo >> "message" README.md
 
@@ -9,11 +9,7 @@ echo "Enter your message"
 read message
 git add .
 git commit -m"${message}"
-#if [ -n "$(git status - porcelain)" ];
-#then
-# echo "IT IS CLEAN"
-#else
-# git status
+
  echo "Pushing data to remote server!!!"
  git push -u origin master
-#fi
+
